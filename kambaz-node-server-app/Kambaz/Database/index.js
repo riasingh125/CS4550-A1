94e9de908c289ada3d689c2aece8a9b72bbdc364
riasingh125 <74634288+riasingh125@users.noreply.github.com>
2025-11-26 17:23:34 -0500

    Delete kambaz-node-server-app directory
    shouldn't be here

--- kambaz-node-server-app/Kambaz/Database/index.js
+++
@@ -1,7 +0,0 @@
-import courses from "./courses.js";
-import modules from "./modules.js";
-import assignments from "./assignments.js";
-import users from "./users.js";
-import enrollments from "./enrollments.js";
-import quizzes from "./quizzes.js";
-export default { courses, modules, assignments, users, enrollments, quizzes };
