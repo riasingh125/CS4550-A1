caff6773491c46513c9257e15a0106b294d1082b
riasingh125 <74634288+riasingh125@users.noreply.github.com>
2025-12-07 22:15:27 -0500

    bruh

--- kambaz-next-js/src/app/(Kambaz)/Courses/[cid]/Quizzes/[quid]/page.tsx
+++ kambaz-next-js/src/app/(Kambaz)/Courses/[cid]/Quizzes/[quid]/page.tsx
@@ -13,6 +13,8 @@ export default async function QuizDetailsPage({
   <><>
       <h3>Ria Singh, Gabriella Mitchell, Rachel Kahn </h3>
       <p>CS 4550, Section 11597 (Friday Section) </p>
+      <p>Github links: Frontend - https://github.com/riasingh125/CS4550-A1/tree/gabi </p>
+      <p>Backend - https://github.com/riasingh125/kambaz-node-server-app/tree/quizzes </p>
     </><QuizDetailsClient cid={cid} quiz={quiz} /></>
   )
 }
