774be979ab2dc463cfa892844e819a575c573cfb
riasingh125 <74634288+riasingh125@users.noreply.github.com>
2025-12-07 22:02:17 -0500

    added name

--- kambaz-next-js/src/app/(Kambaz)/Courses/[cid]/Quizzes/[quid]/page.tsx
+++ kambaz-next-js/src/app/(Kambaz)/Courses/[cid]/Quizzes/[quid]/page.tsx
@@ -9,5 +9,10 @@ export default async function QuizDetailsPage({
   const { cid, quid } = await params;
   const quiz = await findQuizById(quid);
 
-  return <QuizDetailsClient cid={cid} quiz={quiz} />;
+  return ( 
+  <><>
+      <h3>Ria Singh, Gabriella Mitchell, Rachel Kahn </h3>
+      <p>CS 4550, Section 11597 (Friday Section) </p>
+    </><QuizDetailsClient cid={cid} quiz={quiz} /></>
+  )
 }
